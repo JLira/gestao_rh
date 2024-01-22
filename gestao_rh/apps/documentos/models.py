3995e662cb5e766dc616da7a4df61975c1fad67d
jlira <jobson.lira@gmail.com>
2024-01-22 20:18:19 -0300

    chore:relationships

--- gestao_rh/apps/documentos/models.py
+++ gestao_rh/apps/documentos/models.py
@@ -1,9 +1,13 @@
 import uuid
 from django.db import models
 
+from gestao_rh.apps.funcionarios.models import Funcionarios
+
+
 class Documento(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     descricao = models.CharField(max_length=100, help_text='Nome do documento')
-    
+    owner = models.ForeignKey(Funcionarios, on_delete=models.PROTECT )
+
     def __str__(self):
         return f'{self.descricao}'
