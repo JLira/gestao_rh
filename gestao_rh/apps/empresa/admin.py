1078d7e94dc55d9d17772fb2d71893c91cd856c6
jlira <jobson.lira@gmail.com>
2024-01-21 22:48:42 -0300

    chore: company implementation

---
+++ gestao_rh/apps/empresa/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from gestao_rh.apps.empresa.models import Empresa
+
+admin.site.register(Empresa)
+
+
+
---
+++ gestao_rh/apps/empresa/models.py
@@ -0,0 +1,9 @@
+import uuid
+from django.db import models
+
+class Empresa(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, help_text='Nome da Empresa')
+
+    def __str__(self):
+        return f'{self.name}'
